6ec07746dbe810eccdb78c2a11eb0efee3104e4d
Jaime <jtbenavente@gmail.com>
2014-02-21 00:59:05 +0100

    añade los comentarios al codigo

--- geoname.py
+++ geoname.py
@@ -1,15 +1,25 @@
 from lxml import etree
 
+#Abrimos el archivo de las ciudades
 infile = open('ciudades.txt', 'r')
 
+#Hacemos un while con lectura anticipada para leer cada ciudad
 linea=infile.readline()
 
 while linea!="":
+	#Contstruimos la direccion http para realizar la consulta con la ciudad leida
 	http="http://api.geonames.org/search?username=torresj&name="+linea
+	#Convertimos en un "arbol" el archivo xml
 	tree = etree.parse(http)
+
 	geoname = tree.getroot() # elemento raiz
-	ciudad = geoname[1] # Primer hijo
+	
+	# Primer hijo, que coincide con el objeto geoname con la ciudad y sus datos
+	ciudad = geoname[1] 
+	
+	#Varibale para imprimir
 	cadena=""
+	#Recorremos los atributos de la ciudad y usamos solo el nombre y la geoposicion
 	for e in ciudad:
 		if (e.tag == 'name'):
 			cadena=cadena+e.text+" "
@@ -19,11 +29,13 @@ while linea!="":
 			cadena=cadena+e.text+" "
 			print (cadena)
 
+	#Leemos la siguiente ciudad
 	linea=infile.readline()
 
+
+#Cerramos el archivo de las ciudades
 infile.close()
 
-# Los elementos funcionan como listas
     
 
 
